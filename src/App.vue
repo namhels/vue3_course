523e0cf03a97e7831a0f7da2f8ce8152b4129dde
Serhii Lekhman <8.lsv.888@gmail.com>
2023-11-30 18:09:38 +0100

    add buttons likes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
 	<div>
-		<h1>Yahoooooo8</h1>
+		<button @click="addLike">Like</button>
+		<button @click="addDislike">Dislike</button>
+		<div>Number of likes: <strong>{{ likes }}</strong></div>
+		<div>Number of dislikes: <strong>{{ dislikes }}</strong></div>
 	</div>
 </template>
 
 <script>
-	// export default {
-	// 	name: "App"
-	// }
+	export default {
+		name: "App",
+		data() {
+			return {
+				likes: 0,
+				dislikes: 0,
+			}
+		},
+		methods: {
+			addLike () {
+				this.likes += 1
+			},
+			addDislike () {
+				this.dislikes += 1
+			},
+		}
+	}
 </script>
 
 <style lang="scss" scoped>
